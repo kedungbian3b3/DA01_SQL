ab89e27e97706a58660c4fe81c260ecd9833ab7c
kedungbian3b3 <145445490+kedungbian3b3@users.noreply.github.com>
2025-03-17 20:53:49 +0700

    Update Project1.sql

--- Project1.sql
+++ Project1.sql
@@ -1,4 +1,87 @@
 Project1:
+/* 1) Tạo Database ‘Project';
+Dataset: https://drive.google.com/drive/folders/1IWerRguFB0-VXLrIuHmZrfRu3O-H5WXF
+2) Tạo bảng chứa dataset theo câu lệnh sau:
+
+create table SALES_DATASET_RFM_PRJ
+
+(ordernumber VARCHAR,
+
+quantityordered VARCHAR,
+
+priceeach        VARCHAR,
+
+orderlinenumber  VARCHAR,
+
+sales            VARCHAR,
+
+orderdate        VARCHAR,
+
+status           VARCHAR,
+
+productline      VARCHAR,
+
+msrp             VARCHAR,
+
+productcode      VARCHAR,
+
+customername     VARCHAR,
+
+phone            VARCHAR,
+
+addressline1     VARCHAR,
+
+addressline2     VARCHAR,
+
+city             VARCHAR,
+
+state            VARCHAR,
+
+postalcode       VARCHAR,
+
+country          VARCHAR,
+
+territory        VARCHAR,
+
+contactfullname  VARCHAR,
+
+dealsize         VARCHAR
+
+) 
+
+3) Import dữ liệu từ file CSV vào bảng SALES_DATASET_RFM_PRJ vừa tạo
+
+(dataset này sau khi làm sạch sẽ được sử dụng lại ở PROJECT 3)
+
+Lưu ý:
+
+Nếu sales_data_clean_rfm.csv import bị lỗi thì thử với file còn lại  sales_data_clean_rfm_bk.csv
+
+Bước 2: 
+
+Lưu ý: với lệnh DELETE ko nên chạy trước khi bài được review
+
+Hãy làm sạch dữ liệu theo hướng dẫn sau:
+
+Chuyển đổi kiểu dữ liệu phù hợp cho các trường ( sử dụng câu lệnh ALTER) 
+
+Check NULL/BLANK (‘’)  ở các trường: ORDERNUMBER, QUANTITYORDERED, PRICEEACH, ORDERLINENUMBER, SALES, ORDERDATE.
+
+Thêm cột CONTACTLASTNAME, CONTACTFIRSTNAME được tách ra từ CONTACTFULLNAME . 
+
+Chuẩn hóa CONTACTLASTNAME, CONTACTFIRSTNAME theo định dạng chữ cái đầu tiên viết hoa, chữ cái tiếp theo viết thường. 
+
+Gợi ý: ( ADD column sau đó UPDATE)
+
+Thêm cột QTR_ID, MONTH_ID, YEAR_ID lần lượt là Qúy, tháng, năm được lấy ra từ ORDERDATE 
+
+Hãy tìm outlier (nếu có) cho cột QUANTITYORDERED và hãy chọn cách xử lý cho bản ghi đó (2 cách) ( Không chạy câu lệnh trước khi bài được review)
+
+Sau khi làm sạch dữ liệu, hãy lưu vào bảng mới  tên là SALES_DATASET_RFM_PRJ_CLEAN */
+
+	
+	
+	
 --Tạo bảng
 CREATE TABLE SALES_DATASET_RFM_PRJ (
     ordernumber VARCHAR,
