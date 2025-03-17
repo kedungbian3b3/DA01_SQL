1bb5709efcb9ec28eeaf4181bf2281cc56e12e81
kedungbian3b3 <145445490+kedungbian3b3@users.noreply.github.com>
2025-03-17 20:58:54 +0700

    Update project3.sql

--- project3.sql
+++ project3.sql
@@ -1,3 +1,30 @@
+/* Ở Project này chúng ta sử dụng dataset đã được xử lý ở PROJECT 1.
+
+Sau khi dữ liệu được làm sạch hãy tiến hành phân tích theo các gợi ý sau:
+
+1) Doanh thu theo từng ProductLine, Year  và DealSize?
+
+Output: PRODUCTLINE, YEAR_ID, DEALSIZE, REVENUE
+
+2) Đâu là tháng có bán tốt nhất mỗi năm?
+
+Output: MONTH_ID, REVENUE, ORDER_NUMBER
+
+3) Product line nào được bán nhiều ở tháng 11?
+
+Output: MONTH_ID, REVENUE, ORDER_NUMBER
+
+4) Đâu là sản phẩm có doanh thu tốt nhất ở UK mỗi năm? 
+
+Xếp hạng các các doanh thu đó theo từng năm.
+
+Output: YEAR_ID, PRODUCTLINE,REVENUE, RANK
+
+5) Ai là khách hàng tốt nhất, phân tích dựa vào RFM 
+
+(sử dụng lại bảng customer_segment ở buổi học 23) */
+
+
 select * from public.sales_dataset_rfm_prj_clean
 
 /*1) Doanh thu theo từng ProductLine, Year  và DealSize?
